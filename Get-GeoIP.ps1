ff596bd087a5fb82f9539c7558a8bad8ac022878
Darkbat91 <MicahJoiner+Git@gmail.com>
2017-06-01 23:10:41 -0400

    Init

---
+++ Get-GeoIP.ps1
@@ -0,0 +1,48 @@
+﻿Function Get-GeoIP {
+[cmdletbinding()]
+param (
+    [parameter(ValueFromPipeline)]
+    [string[]]$IP 
+)
+Begin
+{
+   $return = @()
+}
+PROCESS
+{
+ 
+    foreach($item in $IP)
+        {
+        try
+           { # Try sending whatever we have
+                $result = Invoke-WebRequest "http://freegeoip.net/xml/$($item)" -ErrorAction STOP
+            }
+        catch  
+            {
+            Write-Verbose "Well $Item didnt work lets try resolving it local"
+                #REsolve the IP local add it to an array and take the first element incase we have multiple return
+            [array]$item = (([System.Net.DNS]::GetHostAddresses("$item")).ipaddresstostring)
+            try
+                {
+                $result = Invoke-WebRequest "http://freegeoip.net/xml/$($item[0])" -ErrorAction STOP
+                }
+            catch
+                {
+                Write-Warning "Cant resolve $($item[0])"
+                }
+            }
+        $result = [xml]$result.Content
+        $val = @{
+            Name = $item
+            Latitude = $result.Response.latitude
+            Longitude = $result.Response.longitude
+        }
+        $return = new-object -TypeName PSOBJECT -Property $val
+        Write-Output -InputObject $return
+        }
+}
+END
+{
+
+}
+}
---
+++ Out-MappedData.ps1
@@ -0,0 +1,136 @@
+﻿function Out-MappedData
+{
+param(
+    [parameter(Mandatory=$true,
+    ValueFromPipeline=$true)]
+[object[]]$data,
+[ValidateScript({Test-Path $_ -PathType Container})] 
+$path = $env:temp,
+$Height=800,
+$Width= 800,
+[switch]$group,
+[string]$filename ='Data.html',
+[switch]$quiet)
+BEGIN
+{
+    $Markers = "var markers = [`n"
+    $count = 0
+}
+
+PROCESS
+{
+    ForEach ($Item in $data)
+    {   $Name =$item.Name.Replace("`'","\'")
+        $Markers += "    ['$Name',$($item.Latitude),$($Item.Longitude)]"
+        $Markers += ",`n"
+        $count++
+    
+    }
+}
+END
+{
+$markers = $markers.TrimEnd(",`n")
+$Markers += "`n  ];`n"
+
+
+$HTML = @"
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>$filename</title>
+    <style>
+      html, body, #map-canvas {
+        height: $($Height)px;
+        width: $($Width)px;
+        margin: 0px;
+        padding: 0px
+      }
+    </style>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script>
+function initialize() {
+  var bounds = new google.maps.LatLngBounds();
+  var mapOptions = {
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  $Markers
+  var datapoints = []
+
+  for( i = 0; i < markers.length; i++ ) {
+    var loc = new google.maps.LatLng(markers[i][1], markers[i][2]);
+
+    var image = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + (i + 1) + '|FF0000|000000';
+    marker = new google.maps.Marker({
+      position: loc,
+"@
+if($group.isPresent)
+    {
+    $html += @"
+      title: markers[i][0]
+    });
+      datapoints.push(marker);
+      bounds.extend(loc);
+  }
+  
+ var markerCluster = new MarkerClusterer(map, datapoints,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+"@
+    }
+else
+    {
+    $html += @"
+    map: map,
+    title: markers[i][0]
+    });
+
+    bounds.extend(loc);
+    }
+"@
+    }
+$html += @"
+    
+
+ 
+  if (markers.length > 1) {
+    map.fitBounds(bounds);
+  }
+  else {
+    map.setCenter(new google.maps.LatLng(markers[0][1],markers[0][2]));
+    map.setZoom(4);
+  }
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
+  <body>
+    <div id="map-canvas"></div><br>
+    <p>Datapoints displayed: $count<br>
+    </p>
+  </body>
+</html>
+"@
+
+Try {
+    $HTML | Out-File "$Path\$filename" -Encoding ASCII -ErrorAction Stop
+}
+Catch {
+    Write-Warning "Unable to save HTML at $Path\$filename because $_"
+    Exit
+}
+
+if($quiet.ispresent -eq $false)
+    {
+    $output = "$Path\$filename"
+    & $output
+    }
+}
+
+}
+
